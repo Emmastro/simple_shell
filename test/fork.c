b9ff922da6ead0c1588d61a8d9614da8c342ddc9
Emmastro <emurairi18@alastudents.org>
2020-11-17 22:55:03 +0100

    Make learning tests

---
+++ test/fork.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/**
+ * main - fork example
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    pid_t my_pid;
+    pid_t child_pid;
+    int status;
+
+    child_pid = fork();
+    if (child_pid == -1)
+    {
+        perror("Error:");
+        return (1);
+    }
+    my_pid = getpid();
+    printf("My pid is >> %u\n", my_pid);
+    if (child_pid == 0)
+    {
+        printf("(%u) Nooooooooo!\n", my_pid);
+    }
+    else
+    {
+        wait(&status);
+        printf("(%u) %u, I am your father\n", my_pid, child_pid);
+    }
+    return (0);
+}
---
+++ test/fork_wait_execve.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * execute - executes the command
+ * @forked: number of child processes
+ * Return: Always 0, -1 on error.
+ */
+int execute(int *forked)
+{
+	int status;
+	pid_t child_pid;
+	pid_t my_pid;
+	char *argv[] = {"/bin/ls", "-l", "/tmp", NULL};
+
+	if (*forked != 5)
+	{
+		child_pid = fork();
+		int status;
+		pid_t child_ppid;
+
+		if (child_pid == -1)
+		{
+			perror("Error:");
+			return (1);
+		}
+		else if (child_pid == 0)
+		{
+			if (execve(argv[0], argv, NULL) == -1)
+				perror("Error:");
+		}
+		else
+		{
+			wait(&status);
+			*forked += 1;
+			execute(forked);
+		}
+	}
+	my_pid = getpid();
+
+	return (0);
+}
+
+/**
+ * main - fork & wait example
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int forked = 0;
+
+	execute(&forked);
+	return (0);
+}
