84bb7fa461ce594372e07ce50c244b09ce1f36b2
Emmastro <emurairi18@alastudents.org>
2020-11-26 07:45:29 +0100

    Fix forbids mixed declarations and code

--- shell.c
+++ shell.c
@@ -9,30 +9,28 @@ int execute(char **cmd)
 {
 
 	pid_t child_pid;
+	int status;
 
 	if (strncmp("exit", cmd[0], 4) == 0)
 		return (-1);
 
 	child_pid = fork();
-	int status;
 
 	if (child_pid == -1)
 	{
-		perror("Error:");
+		perror("Error");
 		return (1);
 	}
 	else if (child_pid == 0)
 	{
 		if (execve(cmd[0], cmd, NULL) == -1)
 		{
-			perror("Error:");
+			perror("Error");
 			exit(-1);
 		}
 	}
 	else
-	{
 		wait(&status);
-	}
 
 	return (0);
 }
@@ -49,22 +47,21 @@ int main(int argc, char **argv)
 {
 
 	int response;
+	char **tokens;
+	size_t bufsize = BUFSIZ;
+	int isPipe = 0;
+	char *buffer;
 
 	if (argc >= 2)
 	{
 		/*TODO: Handle cases where there is no argument, only the command*/
 		if (execve(argv[1], argv, NULL) == -1)
 		{
-			perror("Error:");
+			perror("Error");
 			exit(-1);
 		}
 		return (0);
 	}
-
-	char **tokens;
-	size_t bufsize = BUFSIZ;
-	int isPipe = 0;
-	char *buffer;
 
 	buffer = (char *)malloc(bufsize * sizeof(char));
 	if (buffer == NULL)
